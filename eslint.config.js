60ed4ae04128c9f3c182d44ab39b73d8522bce67
ErinFlyingSkyRocket <144037916+ErinFlyingSkyRocket@users.noreply.github.com>
2025-07-24 21:18:04 +0800

    add back eslint.config.js + permission fix + eslint.yml read config file

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    plugins: {
+      js
+    },
+    rules: {
+      ...js.configs.recommended.rules
+    }
+  },
+
+  // React-specific config
+  pluginReact.configs.flat.recommended,
+
+  // Mocha test globals for test.js
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha
+      }
+    }
+  }
+]);
